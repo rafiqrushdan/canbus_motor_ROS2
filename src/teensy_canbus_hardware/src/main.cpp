2eaa581287edb5a75fc0393b828fbd26c7184c0a
rafiqrushdan <rafiq.wmr@live.iium.edu.my>
2024-10-24 17:32:59 +0800

    update mks42_interfaces for std.message

--- src/teensy_canbus_hardware/src/main.cpp
+++ src/teensy_canbus_hardware/src/main.cpp
@@ -2,21 +2,61 @@
 #include <math.h>
 #include <Arduino.h>
 #include <mks42d_can.h>
-#include <micro_ros_arduino.h>
+#include <micro_ros_platformio.h>
 #include <stdio.h>
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/float32.h>
+
 
 #define CCW 0
 #define CW 1
 
+
+#ifndef RCCHECK
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){rclErrorLoop();}}
+#endif
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)
+
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
+
+
+//Define publisher of the motor
+rcl_publisher_t pub_rpm;
+rcl_publisher_t pub_pulse;
+rcl_publisher_t pub_angle;
+
+
+//Define subscriber of the motor
+rcl_subscription_t motor_rpm;
+rcl_subscription_t angle_motor;
+rcl_subscription_t accel_motor;
+rcl_subscription_t id_can;
+
+
+
+std_msgs__msg__Int32 msg;
 
 CAN_message_t msg_sent;
 CAN_message_t msg_receive;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
 
 struct motor_values {
   double angle;   // in radians
@@ -37,6 +77,7 @@ void setup() {
   can1.setBaudRate(250000);
   Serial.begin(115200);
   motor1.angle = getEncoderVal(0x01); // Get the initial encoder value
+
 }
 
 void loop() {
@@ -60,5 +101,4 @@ void loop() {
 
   calibrate(0x01);
 
-  delay(100);
 }
